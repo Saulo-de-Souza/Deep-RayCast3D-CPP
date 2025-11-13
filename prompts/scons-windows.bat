8cc56cc8dfe69436199897810dce059003de52b5
Saulo-de-Souza <saulodevbr@gmail.com>
2025-11-13 01:45:43 -0300

    21375

--- prompts/scons-windows.bat
+++ prompts/scons-windows.bat
@@ -33,5 +33,10 @@ scons platform=windows arch=x86_64 target=template_release
 scons platform=windows arch=x86_32 target=template_release
 
 echo =====================================================================
+echo Copiando pata addons para o projeto example...
+echo =====================================================================
+xcopy ".\addons\" ".\example" /E /I /Y /Q
+
+echo =====================================================================
 echo Compilação windows finalizada!
 echo =====================================================================
