24298866dec3ea42763307136b4499098a414e7e
Saulo-de-Souza <saulodevbr@gmail.com>
2025-11-13 01:33:41 -0300

    19877

--- prompts/docs.bat
+++ prompts/docs.bat
@@ -1,3 +1,3 @@
 @echo off
-cd "my_game_here" && "D:\5-Programas\Godot\Godot_v4.6-dev3\Godot_v4.6-dev3_win64.exe" --doctool ../ --gdextension-docs
+cd "example" && "D:\5-Programas\Godot\Godot_v4.6-dev3\Godot_v4.6-dev3_win64.exe" --doctool ../ --gdextension-docs
 cd ..
